603c1d7b13c41b3ee63310cc87aa823dfd8ce703
노시온 <shtldhs123@naver.com>
2024-12-23 15:50:58 +0900

    init commit

---
+++ src/main/java/com/hhplus/hhplus_special_course/HhplusSpecialCourseApplication.java
@@ -0,0 +1,13 @@
+package com.hhplus.hhplus_special_course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HhplusSpecialCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HhplusSpecialCourseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hhplus/hhplus_special_course/HhplusSpecialCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hhplus.hhplus_special_course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HhplusSpecialCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
